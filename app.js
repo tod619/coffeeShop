b543334d3d6db2aa73b76b3d2b6a426a74e5e993
tod619 <trevorodwyer619@hotmail.com>
2020-06-02 16:18:04 +0100

    Add naviagtaion event + create an event function to call all events in app.js file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-// Hide preloader
-// All images and scripts have loaded
+function eventListners() {
+  // Hide preloader
+  // All images and scripts have loaded
 
-// add event listner to window
-window.addEventListener("load", () => {
-  document.querySelector(".preloader").style.display = "none";
-});
+  // add event listner to window
+  window.addEventListener("load", () => {
+    document.querySelector(".preloader").style.display = "none";
+  });
+
+  // navigation btn
+  document.querySelector(".navBtn").addEventListener("click", () => {
+    document.querySelector(".nav").classList.toggle("nav--show");
+  });
+}
+
+eventListners();
