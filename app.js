048ef9dd26b1aff9e92b2e2dc02cbe00a14ce8f4
tod619 <trevorodwyer619@hotmail.com>
2020-06-02 15:50:36 +0100

    Add Javascritpt to get the preloader to work in app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Hide preloader
+// All images and scripts have loaded
+
+// add event listner to window
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").style.display = "none";
+});
