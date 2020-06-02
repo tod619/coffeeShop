498146c545a0f3653749ca2852b0f464221f1c4a
tod619 <trevorodwyer619@hotmail.com>
2020-06-02 16:44:25 +0100

    Add toggle event + effect to video__control-btn in style.css + app.js file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function eventListners() {
   const ui = new UI();
 
-  // add event listner to window
+  // add event listner to window for preloader
   window.addEventListener("load", () => {
     ui.hidePreLoader();
   });
@@ -10,16 +10,30 @@ function eventListners() {
   document.querySelector(".navBtn").addEventListener("click", () => {
     ui.showNav();
   });
+
+  //   control for video playback
+  document.querySelector(".video__switch").addEventListener("click", () => {
+    ui.videoControls();
+  });
 }
 
 function UI() {}
 
+// Hide preloader
 UI.prototype.hidePreLoader = function () {
   document.querySelector(".preloader").style.display = "none";
 };
 
+// Show + hide nav menu
 UI.prototype.showNav = function () {
   document.querySelector(".nav").classList.toggle("nav--show");
+};
+
+//control video
+UI.prototype.videoControls = function () {
+  document
+    .querySelector(".video__switch-btn")
+    .classList.toggle("switch-btn-toggle");
 };
 
 eventListners();
